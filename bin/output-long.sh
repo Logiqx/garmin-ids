ca7eca49308ca6c611d3f6033f1378f98ccde018
Logiqx <software@mikeg.me.uk>
2026-06-01 21:52:58 +0100

    Add first cut of long output

---
+++ bin/output-long.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+
+RAW=data/raw/api/*.json
+
+TMP=data/out/long/legal.tmp
+OUT=data/out/long/legal.psv
+
+# Extract part numbers and product names from JSON
+jq -r '.[] | (.partNumber + "|" + .name), (.partNumber + "|" + .additionalNames[])' $RAW >$TMP
+
+# Fix additional product names which have been split in the JSON
+grep -E -v '\|AMOLED|\|Solar|\|Tactical' $TMP >$OUT
+sed -E -i 's/^(006-B4587-00.Instinct® 3 – 50 mm)$/\1, AMOLED, Tactical/' $OUT
+sed -E -i 's/^(006-B4759-00.Instinct® 3 – 50 mm)$/\1, Solar, Tactical/' $OUT
+sed -E -i 's/^(006-B4775-00.tactix® 8 – 51 mm)$/\1, AMOLED/' $OUT
+sed -E -i 's/^(006-B4776-00.tactix® 8 – 51 mm)$/\1, Solar/' $OUT
+
+# Ensure the output is sorted and remove the temporary file
+sort -o $OUT $OUT
+rm $TMP
