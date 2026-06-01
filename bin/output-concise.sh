0468efb477bbcfba85ae7775e0bcce4a07dc5862
Logiqx <software@mikeg.me.uk>
2026-06-01 18:52:00 +0100

    Remove temporary file

--- bin/output-concise.sh
+++ bin/output-concise.sh
@@ -7,12 +7,15 @@ TMP=data/out/concise/legal.tmp
 OUT=data/out/concise/legal.psv
 
 # Extract part numbers and product names from JSON
-jq -r '.[] | (.partNumber + "|" + .name)' $RAW >$TMP
+jq -r '.[] | .partNumber + "|" + .name' $RAW >$TMP
 
 # Determine list of part numbers where the product name will be replaced
 PARTS=$(cut -d '|' -f1 $REF | paste -sd '|' -)
 
-# Replace some product names from the API with concise values, then sort
+# Replace some product names from the API with concise values
 grep -E -v "$PARTS" $TMP >$OUT
 cat $REF >>$OUT
+
+# Ensure the output is sorted and remove the temporary file
 sort -o $OUT $OUT
+rm $TMP
