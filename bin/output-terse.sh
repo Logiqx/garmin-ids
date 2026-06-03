a334dd672bcd041f427957b5a7d4d75855818e0e
Logiqx <software@mikeg.me.uk>
2026-06-03 13:16:07 +0100

    Remove redundant code

--- bin/output-terse.sh
+++ bin/output-terse.sh
@@ -7,9 +7,6 @@ OUT=data/out/terse/legal.psv
 # Extract part numbers and product names from JSON
 jq -r '.[] | .partNumber + "|" + .name' $RAW >$OUT
 
-# Remove the suffix "Carbon"
-sed -i 's/ - Carbon//' $OUT
-
 # Remove the phrase "(No Wi-Fi®)"
 sed -i 's/ (No Wi-Fi®)//' $OUT
 
